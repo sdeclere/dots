b920e01bf89c1a195176e8f65027c94406359630
fat_pinguin <sdeclere@pasteur.fr>
2023-11-09 15:59:47 +0100

    add dashboard

--- init.lua
+++ init.lua
@@ -60,7 +60,19 @@ require("lazy").setup({
 			})
 		end,
 	},
-	
+
+	-- dashboard 	
+	{
+		'nvimdev/dashboard-nvim',
+		event = 'VimEnter',
+		config = function()
+			require('dashboard').setup {
+				-- config
+			}
+		end,
+		dependencies = { {'nvim-tree/nvim-web-devicons'}}
+	}
+
 
 }) -- end of plugins 
 
